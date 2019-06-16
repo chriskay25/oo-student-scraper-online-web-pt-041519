8d322cf5ca653f200bb55822b41c1cf7962bb043
Chris Kay <chriskay25@gmail.com>
2019-06-16 18:22:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,9 +10,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    # binding.pry
-    students_array
-    
+    students_array.each do |student|
+      binding.pry
+      self.new(:name)
+    end 
   end
 
   def add_student_attributes(attributes_hash)
