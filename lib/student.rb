e96a989cb667e258426172a5c3f7701b1ae47905
Chris Kay <chriskay25@gmail.com>
2019-06-16 18:38:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       binding.pry
-      self.new(:name)
+      self.new(student[:name])
     end 
   end
 
